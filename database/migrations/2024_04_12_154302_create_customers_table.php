ad2409b50e18cd130a021dba6a87b8693cc3f7ef
Sebastian Rodriguez Camilo <segarodriguezcamilo@gmail.com>
2024-04-12 10:45:30 -0500

    customers_table

---
+++ database/migrations/2024_04_12_154302_create_customers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('document_number', 15)->unique();
+            $table->string('first_name', 50);
+            $table->string('last_name', 50);
+            $table->string('address', 80)->nullable();
+            $table->date('birthday')->nullable();
+            $table->string('phone_number', 16)->nullable();
+            $table->string('email', 100)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+};
