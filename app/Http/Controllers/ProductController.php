896555159ea304e5f2e1e75a2b3984af1d517200
camilocaicedocarlosfabian40@gmail.com <camilocaicedocarlosfabian40@gmail.com>
2024-04-26 20:27:33 -0500

    ProductController

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = DB::table('products')->get();
+    
+        return view('product.index', ['products' => $products]);
+    }
+    
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('product.create');
+    }
+    
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        DB::table('products')->insert([
+            'name' => $request->name,
+            'price' => $request->price,
+            'stock' => $request->stock,
+            'category_id' => $request->category_id,
+        ]);
+        
+        return redirect()->route('product.index')->with('success', 'Product created successfully.');
+    }
+    
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = DB::table('products')->where('id', $id)->first();
+        
+        return view('product.show', ['product' => $product]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $product = DB::table('products')->where('id', $id)->first();
+        
+        return view('product.edit', ['product' => $product]);
+    }
+    
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id) 
+    {
+        DB::table('products')
+            ->where('id', $id)
+            ->update([
+                'name' => $request->name,
+                'price' => $request->price,
+                'stock' => $request->stock,
+                'category_id' => $request->category_id,
+            ]);
+    
+        return redirect()->route('product.index')->with('success', 'Product updated successfully.');
+    }
+    
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id) 
+    {
+        DB::table('products')->where('id', $id)->delete();
+    
+        return redirect()->route('product.index')->with('success', 'Product deleted successfully.');
+    }
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+use App\Http\Controllers\ProductController;
+
+
+
 require __DIR__.'/auth.php';
