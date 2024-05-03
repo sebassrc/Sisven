0978068b3ef9b513d868665608507677be1c002d
Emmanuel Valencia <emmanuelvalencia479@gmail.com>
2024-05-03 00:25:19 -0500

    Creacion Modelo - Controlador

---
+++ app/Models/Detalle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Detalle extends Model
+{
+    use HasFactory;
+    protected $table = 'details';
+    protected $primaryKey = 'id';
+}
---
+++ database/migrations/2024_05_03_050848_create_details_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('invoice_id');
+            $table->unsignedBigInteger('product_id'); // referencias FK
+            $table->integer('quantity');
+            $table->integer('price');
+            $table->timestamps();
+
+            //definir llave foranea
+
+            $table->foreign('product_id')->references('id')->on('products');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('details');
+    }
+};
