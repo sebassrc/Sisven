3fe5f2190911ef8707b31c255af2804b9fdce3d9
Carlos Fabian Camilo Caicedo <camilocaicedocarlosfabian40@gmail.com>
2024-05-20 20:26:01 -0500

    api Controller products

---
+++ app/Http/Controllers/api/ProductsController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = DB::table('products')->get();
+        return json_encode(['products' => $products]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $product = new Product();
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->price = $request->price;  // Asumiendo que también quieres manejar el precio del producto.
+        $product->save();
+        return json_encode(['product' => $product]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = Product::find($id);
+        return json_encode(['product' => $product]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->price = $request->price;  // Actualizar el precio también, si se proporciona.
+        $product->save();
+        return json_encode(['product' => $product]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+        $product->delete();
+        $products = DB::table('products')->get();
+        return json_encode(['products' => $products, 'success' => true]);
+    }
+}
