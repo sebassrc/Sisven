a7f0821e54bf9f29e6dca15c717317bfe51b4d97
Carlos Fabian Camilo Caicedo <camilocaicedocarlosfabian40@gmail.com>
2024-05-22 09:56:50 -0500

    rutas api productsController

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// Rutas para Products
+Route::get('/products', [ProductsController::class, 'index'])->name('products.index');
+Route::post('/products', [ProductsController::class, 'store'])->name('products.store');
+Route::get('/products/{id}', [ProductsController::class, 'show'])->name('products.show');
+Route::put('/products/{id}', [ProductsController::class, 'update'])->name('products.update');
+Route::delete('/products/{id}', [ProductsController::class, 'destroy'])->name('products.destroy');
